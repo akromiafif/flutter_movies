768505c228891366eb39df342cd25c1da1b0002b
Afif Akromi <massachusetts48@gmail.com>
2022-11-06 16:28:31 +0700

    init project

---
+++ lib/screens/onboarding_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class OnBoardingScreen extends StatelessWidget {
+  const OnBoardingScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: SizedBox(
+        width: screenWidth,
+        height: screenHeight,
+        child: Stack(
+          children: const <Widget>[],
+        ),
+      ),
+    );
+  }
+}
